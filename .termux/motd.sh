e6e702c0d74a31bfa8bd438cb34550435cce51ab
TheJarAiCyberCode-Official <thejaraicybercode@gmail.com>
2025-10-20 21:43:50 +0700

    modified:   .termux/motd.sh

--- .termux/motd.sh
+++ .termux/motd.sh
@@ -1,12 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# --- BAGIAN PENGATURAN AWAL ---
-# Memastikan package manager Termux terpasang dengan benar.
 source "/data/data/com.termux/files/usr/bin/termux-setup-package-manager" || exit 1
 
-# --- BAGIAN PESAN UTAMA ---
-# Pesan Selamat Datang (Welcome Message)
-# Memulai variabel motd dengan pesan selamat datang.
 motd="
 \e[30;47mSelamat Datang di Termux!\e[0m
 \e[1mDokumentasi:\e[0m \e[4mhttps://termux.dev/docs\e[0m
@@ -14,9 +9,6 @@ motd="
 \e[1mKomunitas:\e[0m   \e[4mhttps://termux.dev/community\e[0m
 "
 
-# --- BAGIAN INFORMASI PENTING ---
-# Menambahkan Informasi Paket (Package Information)
-# Menambahkan baris baru ke variabel motd yang sudah ada.
 motd+="
 \e[30;47mBekerja dengan paket:\e[0m
 \e[1mMencari:\e[0m      pkg search <query>
@@ -24,10 +16,6 @@ motd+="
 \e[1mMeningkatkan:\e[0m pkg upgrade
 "
 
-# --- BAGIAN REPOSITORI (KHUSUS APT) ---
-# Tampilan ini hanya muncul jika package manager yang digunakan adalah APT.
-# Memeriksa apakah package manager yang digunakan adalah apt.
-# Jika ya, tambahkan informasi repo tambahan.
 if [ "$TERMUX_APP_PACKAGE_MANAGER" = "apt" ]; then
 
     motd+="
@@ -37,8 +25,6 @@ if [ "$TERMUX_APP_PACKAGE_MANAGER" = "apt" ]; then
 "
 fi
 
-# --- BAGIAN PELAPORAN ISU ---
-# Bagian ini juga selalu ditambahkan di bagian akhir.
 motd+="
 Untuk memperbaiki masalah repositori apa pun,
 jalankan perintah '\e[30;47mtermux-change-repo\e[0m'.
@@ -46,6 +32,4 @@ jalankan perintah '\e[30;47mtermux-change-repo\e[0m'.
 Laporkan masalah di \e[4mhttps://termux.dev/issues\e[0m
 "
 
-# --- TAMPILKAN MOTD ---
-# Menampilkan semua pesan yang sudah digabungkan.
 echo -e "$motd"
